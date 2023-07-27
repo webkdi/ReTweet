9c6ba879d1a169077166e5cbb95b157d9cc7ea39
Dimitri Ko <dimitri.korenev@gmail.com>
2023-07-27 12:11:45 +0200

    erwrq

--- routes/twitter.js
+++ routes/twitter.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const tw = require("../components/twitter");
 
 // Define the timeout duration in milliseconds
-const TIMEOUT_DURATION = 15000; // 10 seconds
+const TIMEOUT_DURATION = 15000; // 15 seconds
 
 router.get("/", async (req, res, next) => {
   return res.status(200).json({
@@ -16,14 +16,16 @@ router.post("/tweets", async (req, res) => {
   try {
     // Set up a timer for the specified duration
     const timeout = setTimeout(() => {
-      // If the timeout is reached, return an error response and end the request
+      // If the timeout is reached, return an error response
       res.status(500).json({ error: "Request timed out" });
     }, TIMEOUT_DURATION);
 
-    const { text, mediaType, mediaUrl } = req.body;
-    console.log("Trying to create tweet with", mediaType, text, mediaUrl);
+    const text = req.body.text;
+    const mediaType = req.body.mediaType;
+    const mediaeUrl = req.body.mediaeUrl;
+    console.log("trying to create tweet with", mediaType, text, mediaeUrl);
 
-    // Check if mediaType is available, must be a string, and can only contain "text", "image", or "video"
+    // Check if mediaType is available, must be a string, and can contain "text", "image", or "video"
     if (
       !mediaType ||
       typeof mediaType !== "string" ||
@@ -31,15 +33,13 @@ router.post("/tweets", async (req, res) => {
     ) {
       // Clear the timeout before sending the response to avoid headers already sent error
       clearTimeout(timeout);
-
       return res.status(400).json({
         error:
           "Invalid mediaType. It must be a string and can only be 'text', 'image', or 'video'.",
       });
     }
 
-    const tweet = await tw.tweetPost(text, mediaType, mediaUrl);
-
+    const tweet = await tw.tweetPost(text, mediaType, mediaeUrl);
     // Clear the timeout before sending the response to avoid headers already sent error
     clearTimeout(timeout);
 
@@ -47,7 +47,9 @@ router.post("/tweets", async (req, res) => {
   } catch (error) {
     // If there's an error during tweetPost or other parts of the try block, handle it appropriately
     console.error("Error while creating tweet:", error);
-    res.status(500).json({ error: "An error occurred while creating the tweet." });
+    res
+      .status(500)
+      .json({ error: "An error occurred while creating the tweet." });
   }
 });
 
